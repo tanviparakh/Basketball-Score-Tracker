5d919c596e6f1b68d2085a6410d7126ad9199f6d
tanviparakh <92598917+tanviparakh@users.noreply.github.com>
2024-04-28 00:34:31 +0530

    4 teams vs 2
    changed it to 4 teams

--- index.js
+++ index.js
@@ -1,51 +1,95 @@
 
-let homeTotal = 0
-let guestTotal = 0
+let team1Total = 0
+let team2Total = 0
+let team3Total = 0
+let team4Total = 0
 
 
-let homeScore = document.getElementById("homeScore")
-let guestScore = document.getElementById("guestScore")
+let team1Score = document.getElementById("teamScore1")
+let team2Score = document.getElementById("teamScore2")
+let team3Score = document.getElementById("teamScore3")
+let team4Score = document.getElementById("teamScore4")
 
 
-//Adding points for home
+//Adding points for team 1
 
-function add1PointHome() {
-    homeTotal += 1
-    homeScore.textContent = homeTotal
+function add1PointT1() {
+    team1Total += 1
+    team1Score.textContent = team1Total
 }
 
-function add2PointsHome() {
-    homeTotal += 2
-    homeScore.textContent = homeTotal
+function add2PointsT1() {
+    team1Total += 2
+    team1Score.textContent = team1Total
 }
 
-function add3PointsHome() {
-    homeTotal += 3
-    homeScore.textContent = homeTotal
+function add3PointsT1() {
+    team1Total += 3
+    team1Score.textContent = team1Total
 }
 
-//Adding points for guest
+//Adding points for Team 2
 
-function add1PointGuest() {
-    guestTotal += 1
-    guestScore.textContent = guestTotal
+function add1PointT2() {
+    team2Total += 1
+    team2Score.textContent = team2Total
 }
 
-function add2PointsGuest() {
-    guestTotal += 2
-    guestScore.textContent = guestTotal
+function add2PointsT2() {
+    team2Total += 2
+    team2Score.textContent = team2Total
 }
 
-function add3PointsGuest() {
-    guestTotal += 3
-    guestScore.textContent = guestTotal
+function add3PointsT2() {
+    team2Total += 3
+    team2Score.textContent = team2Total
 }
+
+//Adding points for Team 3
+
+function add1PointT3() {
+    team3Total += 1
+    team3Score.textContent = team3Total
+}
+
+function add2PointsT3() {
+    team3Total += 2
+    team3Score.textContent = team3Total
+}
+
+function add3PointsT3() {
+    team3Total += 3
+    team3Score.textContent = team3Total
+}
+
+
+//Adding points for Team 4
+function add1PointT4() {
+    team4Total += 1
+    team4Score.textContent = team4Total
+}
+
+function add2PointsT4() {
+    team4Total += 2
+    team4Score.textContent = team4Total
+}
+
+function add3PointsT4() {
+    team4Total += 3
+    team4Score.textContent = team4Total
+}
+
+
 
 function resetGame() {
-    homeTotal = 0
-    guestTotal = 0
-    homeScore.textContent = homeTotal
-    guestScore.textContent = guestTotal
+    team1Total = 0
+    team2Total = 0
+    team3Total = 0
+    team4Total = 0
+    team1Score.textContent = team1Total
+    team2Score.textContent = team2Total
+    team3Score.textContent = team3Total
+    team4Score.textContent = team4Total
 }
 
 
